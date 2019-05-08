b97c2604d58a2d669109e789469ca5894602f87a
Atef Akrout <haifa.akrout@esprit.tn>
2019-05-08 11:05:50 +0100

    db connection

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 var mongoose=require('mongoose')
 
-mongoose.connect('mongodb://localhost/labelPlatform1',{ useNewUrlParser: true ,'useCreateIndex': true})
+mongoose.connect('mongodb+srv://feriel:feriel@mongodb01-4il3d.mongodb.net/test',{ useNewUrlParser: true ,'useCreateIndex': true})
     .then(()=>
     {
         console.log("mongoDB connected successfully...!!")
