ea49735007d45ec759e9dac49ba463104437ee01
Atef Akrout <haifa.akrout@esprit.tn>
2019-05-08 21:59:03 +0100

    haifa commit34

--- app.js
+++ app.js
@@ -36,7 +36,13 @@ require('./routes/passport')(passport);
 
 var db= require('./models/db');
 app.use(express.static(__dirname));
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'twig');
